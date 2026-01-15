7108b8a1bc3d15080c03cdd002ea380e7fdb8581
Rakshat.Jogin <01fe24ica061@kletech.ac.in>
2026-01-15 21:39:57 +0530

    asd

--- test_evenoddprime.py
+++ test_evenoddprime.py
@@ -1,6 +1,18 @@
-from evenoddprime import evenodd_,prime_check
+def evenodd_(num):
+    if num %2==0:
+        return "Even"
+    else:
+        return "Odd"
 
-def test():
-    assert evenodd_(4)=="Even"
-def test1():
-    assert prime_check(11)=="Prime"
+def prime_check(num):
+    if num<=1:
+        return "Not Prime"
+
+    for i in range (2,num):
+        if num % i==0:
+            return "Not Prime"
+    return "Prime"
+
+if __name__=="__main__":
+    print(evenodd_(10))
+    print(prime_check(11))
